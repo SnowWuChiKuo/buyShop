202c17bb032e56908884fb32b8910d1ee489ff58
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2023-05-15 22:33:59 +0800

    feat: add index page

---
+++ controllers/product-controllers.js
@@ -0,0 +1,7 @@
+const productController = {
+  getProduct: (req, res) => {
+    return res.render('products')
+  }
+}
+
+module.exports = productController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const productController = require('../controllers/product-controllers')
 
-router.get('/', (req, res) => {
-  res.send('hi')
-})
+
+router.get('/products', productController.getProduct)
+
+
+router.use('/', (req, res) => res.redirect('/products'))
 
 module.exports = router
