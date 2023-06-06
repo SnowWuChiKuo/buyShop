18aaa96381a380aa8d2ac1e7941112fa971c9454
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2023-06-06 12:02:06 +0800

    feat: add edit useradmin and create category page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const upload = require('../../middleware/multer')
 
 const adminController = require('../../controllers/admin-controllers')
+const categoryController = require('../../controllers/category-controllers')
 
 router.get('/products/create', adminController.createProduct)
 router.get('/products/:id/edit', adminController.editProduct)
@@ -11,6 +12,11 @@ router.put('/products/:id', upload.single('image'), adminController.putProduct)
 router.delete('/products/:id', adminController.deleteProduct)
 router.get('/products', adminController.getProducts)
 router.post('/products', upload.single('image'), adminController.postProduct)
+
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
+
+router.get('/categories', categoryController.getCategories)
 
 router.use('/', (req, res) => res.redirect('/admin/products'))
 
