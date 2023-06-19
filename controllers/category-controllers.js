f216471bd197f9c7d2f469414fc4d2847919acb9
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2023-06-19 11:26:09 +0800

    feat: category create

--- controllers/category-controllers.js
+++ controllers/category-controllers.js
@@ -6,6 +6,13 @@ const categoryController = {
     .then(categories => {
       res.render('admin/categories', { categories })
     })
+  },
+  postCategories: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('此類別已創建!')
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 
