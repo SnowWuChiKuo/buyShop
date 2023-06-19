03aff938bcbca01c96c1c6adcca6770fa123509d
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2023-06-19 18:52:22 +0800

    feat: category update

--- controllers/category-controllers.js
+++ controllers/category-controllers.js
@@ -2,15 +2,29 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({ raw: true })
-    .then(categories => {
-      res.render('admin/categories', { categories })
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+    .then(([categories, category]) => {
+      res.render('admin/categories', { categories, category })
     })
   },
   postCategories: (req, res, next) => {
     const { name } = req.body
     if (!name) throw new Error('此類別已創建!')
     return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('此類別已創建!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error('找不到此類別!')
+        return category.update({ name })
+      })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
