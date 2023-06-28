9ec936c62bb4df9baa532a01a1e724b3ff1154be
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2023-06-28 10:36:31 +0800

    feat: add products index page

--- controllers/product-controllers.js
+++ controllers/product-controllers.js
@@ -1,6 +1,21 @@
+const { Product, Category } = require('../models')
+
 const productController = {
   getProduct: (req, res) => {
-    return res.render('products')
+    return Product.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(product => {
+        const data = product.map(p => ({
+          ...p,
+          description: p.description.substring(0, 50)
+        }))
+        return res.render('products', {
+          products: data
+        })
+      })
   }
 }
 
